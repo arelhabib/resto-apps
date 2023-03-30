05baa259c972a48b044879592eaeccc8e53b49e7
heriryan23 <heriryan23@yahoo.co.id>
2023-03-30 21:40:50 +0800

    update

--- controllers/FoodIngredientController.js
+++ controllers/FoodIngredientController.js
@@ -37,7 +37,7 @@ class FIController {
       if (req.headers.accept.search("html") >= 0) {
         return res.redirect("/foods-ingredients");
       }
-
+      // console.log(result);
       res.json(result);
     } catch (err) {
       res.json(err);
